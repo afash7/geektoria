302f697b9964c63c7325aa00d6b016c8ebd1570a
Afash <afashseven@gmail.com>
2024-08-10 22:09:55 +0330

    first

--- geektoria.py
+++ geektoria.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Afash',
+        'title': 'blog post 1',
+        'content': 'first post content',
+        'date_posted': 'apr 22, 2024'
+    },
+    {
+        'author': 'jane doe',
+        'title': 'blog post 2',
+        'content': 'secend post content',
+        'date_posted': 'apr 28, 2024'
+    }
+]
 
 
 @app.route("/")
-def hello():
-    return "<h1> stacked by insight </h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
